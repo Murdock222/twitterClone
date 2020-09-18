24d8fb7c3bc2b2c4ac0755a6bf220a89c1c1b631
Cory Schlaudecker <cory.schlaudecker@gmail.com>
2020-09-17 21:41:49 -0400

    added tweets

--- tweet/views.py
+++ tweet/views.py
@@ -1,2 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from tweet.models import Tweet
+from tweet.forms import NewTweetForm
+from twitteruser.models import TwitterProfile
+from  django.utils import timezone 
 
+
+
+def newTweet(request):
+    if request.method == "POST":
+        form = NewTweetForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            new_tweet = Tweet.object.create(author=TwitterProfile.get(username="username"), text=data.get(text="text"), tweet_time=timezone.now())
+            new_tweet.save()
+            return HttpResponseRedirect(reverse("homepage"))
+    
+    form = NewTweetForm()
+    return render(request, "generic_form.html", {"form": form})
--- twitteruser/views.py
+++ twitteruser/views.py
@@ -8,12 +8,8 @@ from django.views.generic import Templateview
 def index(request):
     return render(request, "index.html")
 
-class SignUpForm(Templateview):
-    def get(self, request):
-        form = SignupForm()
-        return render(request, "generic_form.html", {"form": form})
-
-    def post(self, request):
+def signup_view(request):
+    if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
@@ -21,26 +17,12 @@ class SignUpForm(Templateview):
             login(request, new_user)
             return HttpResponseRedirect(reverse("homepage"))
 
-
-# def signup_view(request):
-#     if request.method == "POST":
-#         form = SignupForm(request.POST)
-#         if form.is_valid():
-#             data = form.cleaned_data
-#             new_user = TwitterProfile.objects.create_user(username=data.get("username"), password=data.get("password"))
-#             login(request, new_user)
-#             return HttpResponseRedirect(reverse("homepage"))
-
-#     form = SignupForm()
-#     return render(request, "generic_form.html", {"form": form})
+    form = SignupForm()
+    return render(request, "generic_form.html", {"form": form})
     
-class LogInForm(Templateview):
 
-    def get(self, request):
-        form = LoginForm()
-        return render(request, "generic_form.html", {"form": form})
-
-    def post(self, request):
+def login_view(request):
+    if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
@@ -52,25 +34,6 @@ class LogInForm(Templateview):
     form = LoginForm()
     return render(request, "login.html", {"form": form})
 
-
-
-
-# def login_view(request):
-#     if request.method == "POST":
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             data = form.cleaned_data
-#             user = authenticate(request, username=data.get("username"), password=data.get("password"))
-#             if user:
-#                 login(request, user)
-#                 return HttpResponseRedirect(request.GET.get('next', reverse("homepage")))
-
-#     form = LoginForm()
-#     return render(request, "login.html", {"form": form})
-
-
-class LogOutView(Templateview):
-
-    def get(self,request):
-        logout(request)
-        return HttpResponseRedirect(reverse("homepage"))
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("homepage"))
